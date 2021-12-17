6aef107cb88b54fffd1e75a378a63aaf6a11c659
brunoleblevec <gourmet1er@neuf.fr>
2021-12-17 15:57:21 +0100

    comit fin

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include<stdio.h>
+
+void main(void)
+{
+    float a = 37.500003F;
+    printf(Bonjour\n");
+    printf("f:%f\n",a);
+    printf("3f:%3f\n",a);
+    printf("3.2%3.2f\n",a);
+
+}
